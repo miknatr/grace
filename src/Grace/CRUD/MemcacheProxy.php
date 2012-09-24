cfe002460615fe065a970e3dd5721c655c771028
Mikhail Natrov <miknatr@gmail.com>
2012-09-25 02:21:21 +0400

    memcache proxy

--- src/Grace/CRUD/MemcacheProxy.php
+++ src/Grace/CRUD/MemcacheProxy.php
@@ -15,36 +15,57 @@ class MemcacheProxy implements CRUDInterface
 {
     private $memcache;
     private $subject;
-    public function __construct(array $config, CRUDInterface $subject)
+    private $ttl = 600;
+    public function __construct(CRUDInterface $subject)
     {
-        //TODO DODODO
-        $this->memcache = '';
         $this->subject  = $subject;
+    }
+    private function getMemcache()
+    {
+        if (empty($this->memcache)) {
+            $this->memcache = new \Memcache;
+            //TODO расхардкодить и вынести в параметры конструктора
+            $this->memcache->connect('localhost', 11211);
+        }
+        return $this->memcache;
     }
     /**
      * @inheritdoc
      */
     public function selectById($table, $id)
     {
+        $memcache = $this->getMemcache();
+        $r = $memcache->get($table . $id);
+        if (!$r) {
+            $r = $this->subject->selectById($table, $id);
+            $memcache->add($table . $id, $r, 0, $this->ttl);
+        }
+        return $r;
     }
     /**
      * @inheritdoc
      */
     public function insertById($table, $id, array $values)
     {
-        $values['id'] = $id;
+        //лучше здесь не кэшировать, чтобы данные прошли через бд, т.к. там возможна некая фильтрация
+        return $this->subject->insertById($table, $id, $values);
     }
     /**
      * @inheritdoc
      */
     public function updateById($table, $id, array $values)
     {
+        //лучше здесь не кэшировать, чтобы данные прошли через бд, т.к. там возможна некая фильтрация
+        $this->getMemcache()->delete($table . $id);
+        return $this->subject->updateById($table, $id, $values);
     }
     /**
      * @inheritdoc
      */
     public function deleteById($table, $id)
     {
+        $this->getMemcache()->delete($table . $id);
+        return $this->subject->deleteById($table, $id);
     }
 }
 
