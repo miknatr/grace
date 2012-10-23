c43e0489bf96cf0c15ca1f16fbdac3f24cc731ff
Mikhail Natrov <miknatr@gmail.com>
2012-10-23 15:11:44 +0400

    memcache adapter

---
+++ src/Grace/Cache/MemcacheAdapter.php
@@ -0,0 +1,48 @@
+<?php
+
+namespace Grace\Cache;
+
+class MemcacheAdapter extends AbstractAdapter
+{
+    private $adapter;
+    private $namespace;
+    private $enabled;
+
+    public function __construct(\Memcache $adapter, $namespace = '', $enabled = true)
+    {
+        $this->adapter = $adapter;
+        $this->namespace = $namespace;
+        $this->enabled = $enabled;
+    }
+    
+    public function get($key, $ttl = null, callable $cacheSetter = null)
+    {
+        $r = false;
+
+        if ($this->enabled) {
+            $r = $this->adapter->get($this->namespace . '__' . $key);
+            if ($r === false && $cacheSetter !== null) {
+                $r = call_user_func($cacheSetter);
+                $this->set($key, $r, $ttl);
+            }
+        } else {
+            if ($cacheSetter !== null) {
+                $r = call_user_func($cacheSetter);
+            }
+        }
+
+        return $r;
+    }
+    public function set($key, $value, $ttl = null)
+    {
+        $this->adapter->set($this->namespace . '__' . $key, $value, 0, $this->parseTtl($ttl));
+    }
+    public function remove($key)
+    {
+        $this->adapter->delete($this->namespace . '__' . $key);
+    }
+    public function clean()
+    {
+        $this->adapter->flush();
+    }
+}
