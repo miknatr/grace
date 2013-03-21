48ad3fef5aef0ddb6a68c63db3ee00b3492c792d
miknatr <miknatr@gmail.com>
2013-03-21 17:26:43 +0400

    Vendors in one folder

--- install_vendors.sh
+++ install_vendors.sh
@@ -1,6 +1,27 @@
-rm -fR ./vendors
-mkdir -p ./vendors/symfony/src/Symfony/Component/
-git clone http://github.com/symfony/ClassLoader.git ./vendors/symfony/src/Symfony/Component/ClassLoader
-rm -fR ./vendors/symfony/src/Symfony/Component/ClassLoader/.git
-git clone http://github.com/symfony/Yaml ./vendors/symfony/src/Symfony/Component/Yaml
-rm -fR ./vendors/symfony/src/Symfony/Component/Yaml/.git
+rm -fR ./vendor
+mkdir -p ./vendor/symfony/src/Symfony/Component/
+git clone http://github.com/symfony/ClassLoader.git ./vendor/symfony/src/Symfony/Component/ClassLoader
+rm -fR ./vendor/symfony/src/Symfony/Component/ClassLoader/.git
+git clone http://github.com/symfony/Yaml ./vendor/symfony/src/Symfony/Component/Yaml
+rm -fR ./vendor/symfony/src/Symfony/Component/Yaml/.git
+
+#!/bin/sh
+[ ! -e vendor ] && mkdir vendor
+cd vendor
+
+rm -fR ./zf1
+
+mkdir -p ./zf1/library/Zend
+svn -q export http://framework.zend.com/svn/framework/standard/trunk/library/Zend/CodeGenerator ./zf1/library/Zend/CodeGenerator
+svn -q export http://framework.zend.com/svn/framework/standard/trunk/library/Zend/Reflection ./zf1/library/Zend/Reflection
+svn -q export http://framework.zend.com/svn/framework/standard/trunk/library/Zend/Exception.php ./zf1/library/Zend/Exception.php
+svn -q export http://framework.zend.com/svn/framework/standard/trunk/library/Zend/Loader ./zf1/library/Zend/Loader
+svn -q export http://framework.zend.com/svn/framework/standard/trunk/library/Zend/Loader.php ./zf1/library/Zend/Loader.php
+
+#remove includes follow manual
+#http://framework.zend.com/manual/en/performance.classloading.html#performance.classloading.striprequires.sed
+cd ./zf1/library/Zend
+find . -name '*.php' -not -wholename '*/Loader/Autoloader.php' \
+-not -wholename '*/Application.php' -print0 | \
+xargs -0 sed --regexp-extended --in-place 's/(require_once)/\/\/ \1/g'
+cd ../../../
--- tests/bootstrap.php
+++ tests/bootstrap.php
@@ -4,12 +4,12 @@ error_reporting(E_ALL | E_STRICT);
 ini_set("display_errors", 1);
 
 
-require_once  __DIR__ . '/../vendors/symfony/src/Symfony/Component/ClassLoader/UniversalClassLoader.php';
+require_once  __DIR__ . '/../vendor/symfony/src/Symfony/Component/ClassLoader/UniversalClassLoader.php';
 $loader = new \Symfony\Component\ClassLoader\UniversalClassLoader();
 $loader->registerNamespaces(array(
-                                 'Symfony'      => __DIR__ . '/../vendors/symfony/src/',
-                                 'Grace'        => array(__DIR__ . '/../src/', __DIR__ . '/../tests/'),
-                            ));
+    'Symfony'      => __DIR__ . '/../vendor/symfony/src/',
+    'Grace'        => array(__DIR__ . '/../src/', __DIR__ . '/../tests/'),
+));
 $loader->register();
 
 require_once __DIR__ . '/config.php';
