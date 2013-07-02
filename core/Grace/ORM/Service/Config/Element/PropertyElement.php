6630ec3e881742cc4c40aa91a2b2728447a587d1
miknatr <miknatr@gmail.com>
2013-07-02 04:04:34 +0400

    Попробовал переделать на резолвах парсинг пропертей

--- core/Grace/ORM/Service/Config/Element/PropertyElement.php
+++ core/Grace/ORM/Service/Config/Element/PropertyElement.php
@@ -10,10 +10,12 @@
 
 namespace Grace\ORM\Service\Config\Element;
 
+use Grace\ORM\Service\Config\Config;
+
 class PropertyElement
 {
     /**
-     * Grace haven't got own validation service
+     * Grace hasn't got own validation service
      * So, validation from config is loaded "as is" and custom validation service has to handle it.
      * @var mixed
      */
@@ -65,4 +67,142 @@ class PropertyElement
      * @var ProxyElement
      */
     public $proxy;
+
+
+
+    //
+    // PARSING
+    //
+
+    /** @var MappingElement  */
+    private $rawMapping;
+    public static function create($modelName, $propertyName, $mapping, $default = null, $validation = null)
+    {
+        $mapping = static::parseMapping($mapping);
+
+        if ($propertyName == 'id' && !$mapping->localPropertyType) {
+            throw new \LogicException("Bad mapping: {$modelName}.{$propertyName} must have a local mapping");
+        }
+
+        $property = new PropertyElement();
+        $property->rawMapping = $mapping;
+
+        if ($validation) {
+            $property->validation = $validation;
+        }
+        if ($default) {
+            $property->default = new DefaultElement($default);
+        }
+
+        $property->isSettable = $mapping->localPropertyType || $mapping->foreignKeyTable;
+        $property->isLocalInDb = $property->isSettable; // in theory isSettable can be different from isLocalInDb
+
+        // we only allow NULL in foreign key properties and proxy-properties (which are null when the foreign key is null)
+        $property->isNullable = $mapping->foreignKeyTable || $mapping->relationLocalProperty;
+
+        $property->resolvesToModelName = $mapping->foreignKeyTable;
+
+        return $property;
+    }
+
+    private static function parseMapping($mapping)
+    {
+        $me = new MappingElement();
+        if (preg_match('/^(\w+):(\w+)$/', $mapping, $match)) {
+            $me->relationLocalProperty = $match[1];
+            $me->relationForeignProperty = $match[2];
+        } elseif ($mapping[0] == '^') {
+            $me->foreignKeyTable = substr($mapping, 1);
+        } elseif ($mapping) {
+            $me->localPropertyType = $mapping;
+        } else {
+            throw new \Exception('bad config: cannot parse mapping');
+        }
+        return $me;
+    }
+
+
+
+    //
+    // RESOLVING
+    //
+
+    public static function resolveConfig(Config $config)
+    {
+        foreach ($config->models as $modelName => $modelConfig) {
+            foreach ($modelConfig->properties as $propertyName => $propertyConfig) {
+                static::resolve($config, $modelName, $propertyName, $propertyConfig); //php is love
+            }
+        }
+    }
+
+    private $isResolved = false;
+    private static function resolve(Config $config, $modelName, $propertyName, PropertyElement $propertyConfig)
+    {
+        if ($propertyConfig->isResolved) {
+            return;
+        }
+
+        $propertyConfig->type  = static::getPropertyType($config, $modelName, $propertyName, $propertyConfig);
+        $propertyConfig->proxy = static::parseProxy($config, $modelName, $propertyName, $propertyConfig);
+
+        if ($propertyConfig->type === null) {
+            throw new \LogicException("Cannot parse type for {$modelName}.{$propertyName}");
+        }
+
+        $propertyConfig->isResolved = true;
+    }
+
+    private static function getPropertyType(Config $config, $modelName, $propName, PropertyElement $propertyConfig)
+    {
+        $mapping = $propertyConfig->rawMapping;
+
+        // local field
+        if ($mapping->localPropertyType) {
+            return $mapping->localPropertyType;
+        }
+
+        // foreign key
+        if ($mapping->foreignKeyTable) {
+            // we need the type of ID of the foreign table
+            $foreignProperty = $config->models[$mapping->foreignKeyTable]->properties['id'];
+            static::resolve($config, $mapping->foreignKeyTable, 'id', $foreignProperty);
+
+            return $foreignProperty->type;
+        }
+
+        // proxy field
+        if ($mapping->relationLocalProperty) {
+            $localProperty = $config->models[$modelName]->properties[$mapping->relationLocalProperty];
+            static::resolve($config, $modelName, $mapping->relationLocalProperty, $localProperty);
+
+            $foreignProperty = $config->models[$localProperty->resolvesToModelName]->properties[$mapping->relationForeignProperty];
+            static::resolve($config, $localProperty->resolvesToModelName, $mapping->relationForeignProperty, $foreignProperty);
+
+            return $foreignProperty->type;
+        }
+
+        throw new \Exception("bad config: HZHZHZHHZHZ $modelName, $propName");
+    }
+
+    private static function parseProxy(Config $config, $modelName, $propName, PropertyElement $propertyConfig)
+    {
+        $mapping = $propertyConfig->rawMapping;
+
+        if (!$mapping->relationLocalProperty) {
+            return null;
+        }
+
+        $localProperty = $config->models[$modelName]->properties[$mapping->relationLocalProperty];
+        static::resolve($config, $modelName, $mapping->relationLocalProperty, $localProperty);
+
+        $proxy = new ProxyElement();
+        $proxy->localField   = $mapping->relationLocalProperty;
+        $proxy->foreignTable = $localProperty->resolvesToModelName;
+        $proxy->foreignField = $mapping->relationForeignProperty;
+
+        $localProperty->dependentProxies[$propName] = $proxy;
+
+        return $proxy;
+    }
 }
--- core/Grace/ORM/Service/Config/Loader.php
+++ core/Grace/ORM/Service/Config/Loader.php
@@ -45,36 +45,18 @@ class Loader
                 if (!isset($propertyConfig['mapping'])) {
                     continue;
                 }
-                $mapping = $this->parseMapping($propertyConfig['mapping']);
 
-                if ($propertyName == 'id' && !$mapping->localPropertyType) {
-                    throw new \LogicException("Bad mapping: {$modelName}.{$propertyName} must have a local mapping");
+                $property = PropertyElement::create(
+                    $modelName,
+                    $propertyName,
+                    $propertyConfig['mapping'],
+                    isset($propertyConfig['default']) ? $propertyConfig['default'] : null,
+                    isset($propertyConfig['validation']) ? $propertyConfig['validation'] : null
+                );
+
+                if (!$property) {
+                    continue;
                 }
-
-                $property = new PropertyElement();
-                if (isset($propertyConfig['validation'])) {
-                    $property->validation = $propertyConfig['validation'];
-                }
-                if (isset($propertyConfig['default'])) {
-                    $property->default = new DefaultElement($propertyConfig['default']);
-                }
-
-                $property->isSettable = $mapping->localPropertyType || $mapping->foreignKeyTable;
-                $property->isLocalInDb = $property->isSettable; // in theory isSettable can be different from isLocalInDb
-
-                $property->type = $this->getPropertyType($array, $modelName, $propertyName);
-                // we only allow NULL in foreign key properties and proxy-properties (which are null when the foreign key is null)
-                $property->isNullable = $mapping->foreignKeyTable || $mapping->relationLocalProperty;
-
-                $property->resolvesToModelName = $mapping->foreignKeyTable;
-                $property->dependentProxies = $this->getDependentProxies($array, $modelName, $propertyName);
-
-                $property->proxy = $this->parseProxy($array, $modelName, $propertyName);
-
-                if ($property->type === null) {
-                    throw new \LogicException("Cannot parse type for {$modelName}.{$propertyName}");
-                }
-
                 $properties[$propertyName] = $property;
             }
 
@@ -92,105 +74,9 @@ class Loader
             $config->models[$modelName] = $model;
         }
 
+        PropertyElement::resolveConfig($config);
+
         return $config;
-    }
-
-    private function parseMapping($mapping)
-    {
-        $me = new MappingElement();
-        if (preg_match('/^(\w+):(\w+)$/', $mapping, $match)) {
-            $me->relationLocalProperty = $match[1];
-            $me->relationForeignProperty = $match[2];
-        } elseif ($mapping[0] == '^') {
-            $me->foreignKeyTable = substr($mapping, 1);
-        } elseif ($mapping) {
-            $me->localPropertyType = $mapping;
-        } else {
-            throw new \Exception('bad config: cannot parse mapping');
-        }
-        return $me;
-    }
-
-    private function getPropertyType($rawConfig, $modelName, $propName)
-    {
-        if (empty($rawConfig['models'][$modelName]['properties'][$propName]['mapping'])) {
-            return null;
-        }
-        $mapping = $this->parseMapping($rawConfig['models'][$modelName]['properties'][$propName]['mapping']);
-
-        // local field
-        if ($mapping->localPropertyType) {
-            return $mapping->localPropertyType;
-        }
-
-        // foreign key
-        if ($mapping->foreignKeyTable) {
-            // we need the type of ID of the foreign table
-            return $this->getPropertyType($rawConfig, $mapping->foreignKeyTable, 'id');
-        }
-
-        // proxy field
-        if ($mapping->relationLocalProperty) {
-            if (empty($rawConfig['models'][$modelName]['properties'][$mapping->relationLocalProperty]['mapping'])) {
-                return null;
-            }
-            $localPropMapping = $this->parseMapping($rawConfig['models'][$modelName]['properties'][$mapping->relationLocalProperty]['mapping']);
-            $foreignModelName = $localPropMapping->foreignKeyTable;
-            if (!$foreignModelName) {
-                throw new \Exception("Bad config, very bad");
-            }
-
-            return $this->getPropertyType($rawConfig, $foreignModelName, $mapping->relationForeignProperty);
-        }
-
-        return null;
-    }
-
-    private function getDependentProxies($rawConfig, $modelName, $propName)
-    {
-        if (empty($rawConfig['models'][$modelName]['properties'][$propName]['mapping'])) {
-            return null;
-        }
-        $mapping = $this->parseMapping($rawConfig['models'][$modelName]['properties'][$propName]['mapping']);
-
-        if (!$mapping->foreignKeyTable) {
-            return array();
-        }
-
-        $list = array();
-        foreach ($rawConfig['models'][$modelName]['properties'] as $iPropName => $rawPropConfig) {
-            $proxy = $this->parseProxy($rawConfig, $modelName, $iPropName);
-            if ($proxy && $proxy->localField == $propName) {
-                $list[$iPropName] = $proxy;
-            }
-        }
-        return $list;
-    }
-
-    private function parseProxy($rawConfig, $modelName, $propName)
-    {
-        if (empty($rawConfig['models'][$modelName]['properties'][$propName]['mapping'])) {
-            return null;
-        }
-        $mapping = $this->parseMapping($rawConfig['models'][$modelName]['properties'][$propName]['mapping']);
-
-        if (!$mapping->relationLocalProperty) {
-            return null;
-        }
-
-        if (empty($rawConfig['models'][$modelName]['properties'][$mapping->relationLocalProperty]['mapping'])) {
-            throw new \LogicException("Bad mapping in {$modelName}.{$propName}: depends on {$modelName}.{$mapping->relationLocalProperty} which has no mapping");
-        }
-        $relationMapping = $this->parseMapping($rawConfig['models'][$modelName]['properties'][$mapping->relationLocalProperty]['mapping']);
-        if (!$relationMapping->foreignKeyTable) {
-            throw new \LogicException("Bad mapping in {$modelName}.{$propName}: we need {$modelName}.{$mapping->relationLocalProperty} to be a foreign key");
-        }
-
-        $proxy = new ProxyElement();
-        $proxy->localField   = $mapping->relationLocalProperty;
-        $proxy->foreignTable = $relationMapping->foreignKeyTable;
-        $proxy->foreignField = $mapping->relationForeignProperty;
-        return $proxy;
     }
 
     private function loadResource($resource)
